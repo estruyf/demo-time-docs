b6eeb12339945ec2a54d3af52ce32f635ada5cd7
Elio Struyf <elio@struyfconsulting.be>
2024-12-17 13:37:38 +0100

    Update config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://estruyf.github.io',
+  base: 'demo-time-astro',
 	integrations: [
 		starlight({
 			logo: {
